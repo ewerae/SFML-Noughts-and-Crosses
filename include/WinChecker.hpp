958922e0361dd9aed24254df7accaeda2eb7c53e
ewerae <138021695+ewerae@users.noreply.github.com>
2023-08-19 14:40:46 +0100

    Update WinChecker.hpp

--- include/WinChecker.hpp
+++ include/WinChecker.hpp
@@ -11,14 +11,14 @@ class WinChecker {
     private:      
         const SymbolState (&grid)[3][3];
 
+        bool CheckRows(SymbolState symbol) const;
+        bool CheckColumns(SymbolState symbol) const;
+        bool CheckDiagonals(SymbolState symbol) const;
+
     public:
         WinChecker(const SymbolState (&boardGrid)[3][3]);
 
         bool CheckWin(SymbolState symbol) const;
         bool CheckDraw() const;
-
-        bool CheckRows(SymbolState symbol) const;
-        bool CheckColumns(SymbolState symbol) const;
-        bool CheckDiagonals(SymbolState symbol) const;
 
 };
